a8916b71bad44f101dc224293acdc859ee0d3832
Lorincz Krisztian <lorinczkriszty@gmail.com>
2023-01-10 15:29:52 +0200

    Can now also draw rectangle

--- Proiect-grafica-pe-calculator/Form1.Designer.cs
+++ Proiect-grafica-pe-calculator/Form1.Designer.cs
@@ -32,8 +32,8 @@
             this.btn_draw = new System.Windows.Forms.Button();
             this.btn_roots = new System.Windows.Forms.Button();
             this.btn_vertex = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.btn_draw_rectangle = new System.Windows.Forms.Button();
+            this.btn_troubleshoot = new System.Windows.Forms.Button();
             this.a_txtbox = new System.Windows.Forms.TextBox();
             this.c_txtbox = new System.Windows.Forms.TextBox();
             this.b_txtbox = new System.Windows.Forms.TextBox();
@@ -41,13 +41,25 @@
             this.lbl_b = new System.Windows.Forms.Label();
             this.lbl_c = new System.Windows.Forms.Label();
             this.lbl_result = new System.Windows.Forms.Label();
+            this.txtbox_colt1x = new System.Windows.Forms.TextBox();
+            this.txtbox_colt1y = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtbox_colt2x = new System.Windows.Forms.TextBox();
+            this.txtbox_colt2y = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.btn_inter = new System.Windows.Forms.Button();
+            this.listbox_result = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // drawing_board
             // 
+            this.drawing_board.BackColor = System.Drawing.Color.White;
             this.drawing_board.Location = new System.Drawing.Point(120, 12);
             this.drawing_board.Name = "drawing_board";
-            this.drawing_board.Size = new System.Drawing.Size(709, 383);
+            this.drawing_board.Size = new System.Drawing.Size(930, 518);
             this.drawing_board.TabIndex = 0;
             this.drawing_board.Paint += new System.Windows.Forms.PaintEventHandler(this.drawing_board_Paint);
             // 
@@ -81,41 +93,43 @@
             this.btn_vertex.UseVisualStyleBackColor = true;
             this.btn_vertex.Click += new System.EventHandler(this.btn_vertex_Click);
             // 
-            // button4
+            // btn_draw_rectangle
             // 
-            this.button4.Location = new System.Drawing.Point(28, 99);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btn_draw_rectangle.Location = new System.Drawing.Point(28, 99);
+            this.btn_draw_rectangle.Name = "btn_draw_rectangle";
+            this.btn_draw_rectangle.Size = new System.Drawing.Size(75, 23);
+            this.btn_draw_rectangle.TabIndex = 4;
+            this.btn_draw_rectangle.Text = "Draw R";
+            this.btn_draw_rectangle.UseVisualStyleBackColor = true;
+            this.btn_draw_rectangle.Click += new System.EventHandler(this.btn_draw_rectangle_Click);
             // 
-            // button5
+            // btn_troubleshoot
             // 
-            this.button5.Location = new System.Drawing.Point(28, 128);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btn_troubleshoot.Location = new System.Drawing.Point(28, 128);
+            this.btn_troubleshoot.Name = "btn_troubleshoot";
+            this.btn_troubleshoot.Size = new System.Drawing.Size(75, 23);
+            this.btn_troubleshoot.TabIndex = 5;
+            this.btn_troubleshoot.Text = "Trblshoot";
+            this.btn_troubleshoot.UseVisualStyleBackColor = true;
+            this.btn_troubleshoot.Click += new System.EventHandler(this.btn_troubleshoot_Click);
             // 
             // a_txtbox
             // 
-            this.a_txtbox.Location = new System.Drawing.Point(164, 415);
+            this.a_txtbox.Location = new System.Drawing.Point(82, 536);
             this.a_txtbox.Name = "a_txtbox";
             this.a_txtbox.Size = new System.Drawing.Size(100, 23);
             this.a_txtbox.TabIndex = 6;
             // 
             // c_txtbox
             // 
-            this.c_txtbox.Location = new System.Drawing.Point(164, 464);
+            this.c_txtbox.Location = new System.Drawing.Point(82, 585);
             this.c_txtbox.Name = "c_txtbox";
             this.c_txtbox.Size = new System.Drawing.Size(100, 23);
             this.c_txtbox.TabIndex = 7;
             // 
             // b_txtbox
             // 
-            this.b_txtbox.Location = new System.Drawing.Point(164, 440);
+            this.b_txtbox.Location = new System.Drawing.Point(82, 561);
             this.b_txtbox.Name = "b_txtbox";
             this.b_txtbox.Size = new System.Drawing.Size(100, 23);
             this.b_txtbox.TabIndex = 8;
@@ -123,7 +137,7 @@
             // lbl_a
             // 
             this.lbl_a.AutoSize = true;
-            this.lbl_a.Location = new System.Drawing.Point(120, 418);
+            this.lbl_a.Location = new System.Drawing.Point(38, 539);
             this.lbl_a.Name = "lbl_a";
             this.lbl_a.Size = new System.Drawing.Size(16, 15);
             this.lbl_a.TabIndex = 9;
@@ -132,7 +146,7 @@
             // lbl_b
             // 
             this.lbl_b.AutoSize = true;
-            this.lbl_b.Location = new System.Drawing.Point(120, 443);
+            this.lbl_b.Location = new System.Drawing.Point(38, 564);
             this.lbl_b.Name = "lbl_b";
             this.lbl_b.Size = new System.Drawing.Size(17, 15);
             this.lbl_b.TabIndex = 10;
@@ -141,7 +155,7 @@
             // lbl_c
             // 
             this.lbl_c.AutoSize = true;
-            this.lbl_c.Location = new System.Drawing.Point(120, 467);
+            this.lbl_c.Location = new System.Drawing.Point(38, 588);
             this.lbl_c.Name = "lbl_c";
             this.lbl_c.Size = new System.Drawing.Size(16, 15);
             this.lbl_c.TabIndex = 11;
@@ -150,17 +164,122 @@
             // lbl_result
             // 
             this.lbl_result.AutoSize = true;
-            this.lbl_result.Location = new System.Drawing.Point(481, 443);
+            this.lbl_result.Location = new System.Drawing.Point(399, 564);
             this.lbl_result.Name = "lbl_result";
             this.lbl_result.Size = new System.Drawing.Size(45, 15);
             this.lbl_result.TabIndex = 12;
             this.lbl_result.Text = "Result: ";
             // 
+            // txtbox_colt1x
+            // 
+            this.txtbox_colt1x.Location = new System.Drawing.Point(269, 536);
+            this.txtbox_colt1x.Name = "txtbox_colt1x";
+            this.txtbox_colt1x.Size = new System.Drawing.Size(100, 23);
+            this.txtbox_colt1x.TabIndex = 13;
+            // 
+            // txtbox_colt1y
+            // 
+            this.txtbox_colt1y.Location = new System.Drawing.Point(269, 561);
+            this.txtbox_colt1y.Name = "txtbox_colt1y";
+            this.txtbox_colt1y.Size = new System.Drawing.Size(100, 23);
+            this.txtbox_colt1y.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(206, 539);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "x colt 1:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 564);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "y colt 1:";
+            // 
+            // txtbox_colt2x
+            // 
+            this.txtbox_colt2x.Location = new System.Drawing.Point(269, 585);
+            this.txtbox_colt2x.Name = "txtbox_colt2x";
+            this.txtbox_colt2x.Size = new System.Drawing.Size(100, 23);
+            this.txtbox_colt2x.TabIndex = 17;
+            // 
+            // txtbox_colt2y
+            // 
+            this.txtbox_colt2y.Location = new System.Drawing.Point(269, 610);
+            this.txtbox_colt2y.Name = "txtbox_colt2y";
+            this.txtbox_colt2y.Size = new System.Drawing.Size(100, 23);
+            this.txtbox_colt2y.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(206, 588);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "x colt 2:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(206, 613);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 15);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "y colt 2:";
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(28, 157);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 21;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // btn_inter
+            // 
+            this.btn_inter.Location = new System.Drawing.Point(28, 186);
+            this.btn_inter.Name = "btn_inter";
+            this.btn_inter.Size = new System.Drawing.Size(75, 23);
+            this.btn_inter.TabIndex = 22;
+            this.btn_inter.Text = "Intersectia";
+            this.btn_inter.UseVisualStyleBackColor = true;
+            this.btn_inter.Click += new System.EventHandler(this.btn_inter_Click);
+            // 
+            // listbox_result
+            // 
+            this.listbox_result.FormattingEnabled = true;
+            this.listbox_result.ItemHeight = 15;
+            this.listbox_result.Location = new System.Drawing.Point(564, 539);
+            this.listbox_result.Name = "listbox_result";
+            this.listbox_result.Size = new System.Drawing.Size(163, 94);
+            this.listbox_result.TabIndex = 23;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(841, 569);
+            this.ClientSize = new System.Drawing.Size(1070, 655);
+            this.Controls.Add(this.listbox_result);
+            this.Controls.Add(this.btn_inter);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtbox_colt2y);
+            this.Controls.Add(this.txtbox_colt2x);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtbox_colt1y);
+            this.Controls.Add(this.txtbox_colt1x);
             this.Controls.Add(this.lbl_result);
             this.Controls.Add(this.lbl_c);
             this.Controls.Add(this.lbl_b);
@@ -168,8 +287,8 @@
             this.Controls.Add(this.b_txtbox);
             this.Controls.Add(this.c_txtbox);
             this.Controls.Add(this.a_txtbox);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btn_troubleshoot);
+            this.Controls.Add(this.btn_draw_rectangle);
             this.Controls.Add(this.btn_vertex);
             this.Controls.Add(this.btn_roots);
             this.Controls.Add(this.btn_draw);
@@ -187,8 +306,8 @@
         private Button btn_draw;
         private Button btn_roots;
         private Button btn_vertex;
-        private Button button4;
-        private Button button5;
+        private Button btn_draw_rectangle;
+        private Button btn_troubleshoot;
         private TextBox a_txtbox;
         private TextBox c_txtbox;
         private TextBox b_txtbox;
@@ -196,5 +315,16 @@
         private Label lbl_b;
         private Label lbl_c;
         private Label lbl_result;
+        private TextBox txtbox_colt1x;
+        private TextBox txtbox_colt1y;
+        private Label label1;
+        private Label label2;
+        private TextBox txtbox_colt2x;
+        private TextBox txtbox_colt2y;
+        private Label label3;
+        private Label label4;
+        private Button btn_clear;
+        private Button btn_inter;
+        private ListBox listbox_result;
     }
 }
--- Proiect-grafica-pe-calculator/Form1.cs
+++ Proiect-grafica-pe-calculator/Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 namespace Proiect_grafica_pe_calculator
 {
     public partial class Form1 : Form
@@ -7,11 +9,13 @@ namespace Proiect_grafica_pe_calculator
             InitializeComponent();
         }
 
-        //calories += Convert.ToDouble(lbl_cal_barley.Text) * (double.Parse(txtbox_barley.Text) / 100);
-
-        
         private void drawing_board_Paint(object sender, PaintEventArgs e)
         {
+            Graphics gra = this.drawing_board.CreateGraphics();
+            Pen blackPen = new Pen(Color.Black, 1);
+
+            e.Graphics.DrawLine(blackPen, drawing_board.Width / 2, 0, drawing_board.Width / 2, drawing_board.Width);
+            e.Graphics.DrawLine(blackPen, 0, drawing_board.Height / 2, drawing_board.Width, drawing_board.Height / 2);
 
         }
 
@@ -54,6 +58,7 @@ namespace Proiect_grafica_pe_calculator
             lbl_result.Text = result;
         }
 
+        // EventArgs e,
         private void btn_draw_Click(object sender, EventArgs e)
         {
             int a = int.Parse(a_txtbox.Text);
@@ -62,26 +67,147 @@ namespace Proiect_grafica_pe_calculator
             double x;
             double y;
 
+            double vertex_x = -double.Parse(b_txtbox.Text) / (2 * double.Parse(a_txtbox.Text));
+            double vertex_y = double.Parse(a_txtbox.Text) * Math.Pow(vertex_x, 2) + double.Parse(b_txtbox.Text) * vertex_x + double.Parse(c_txtbox.Text);
 
-            int origo_x = drawing_board.Width / 2;
-            int origo_y = drawing_board.Height/ 2;
+            int origin_x = drawing_board.Width / 2 + Convert.ToInt32(vertex_x);
+            int origin_y = drawing_board.Height/ 2 - Convert.ToInt32(vertex_y);
 
             Bitmap pen = new Bitmap(1, 1);
             pen.SetPixel(0, 0, Color.Black);    
             System.Drawing.Graphics g = drawing_board.CreateGraphics();
-
-            for(double i = 0; i <= 10000; i += 0.01)
+            
+            for (double i = vertex_x; i <= 10000; i += 0.01)
             {
                 x = i;
                 y = (a * (x * x)) + ((b * x) + c);
 
-                g.DrawImageUnscaled(pen, origo_x + (int)x, origo_y - (int)y);
-                g.DrawImageUnscaled(pen, origo_x - (int)x, origo_y - (int)y);
+                g.DrawImageUnscaled(pen, origin_x + (int)x, origin_y - (int)y);
+                g.DrawImageUnscaled(pen, origin_x - (int)x, origin_y - (int)y);
 
-                if (y >= origo_y)
+                if (y >= origin_y)
                     break;
             }
-            
+        }
+
+        private void btn_draw_rectangle_Click(object sender, EventArgs e)
+        {
+            System.Drawing.Graphics g = drawing_board.CreateGraphics();
+
+            Pen blackPen = new Pen(Color.Black, 1);
+
+            int xcolt1 = Convert.ToInt32(txtbox_colt1x.Text) + (drawing_board.Width / 2);
+            int ycolt1 = (drawing_board.Height / 2) - Convert.ToInt32(txtbox_colt1y.Text);
+
+            int xcolt2 = Convert.ToInt32(txtbox_colt2x.Text) + (drawing_board.Width / 2);
+            int ycolt2 = (drawing_board.Height / 2) - Convert.ToInt32(txtbox_colt2y.Text);
+
+            int xcolt3 = xcolt1;
+            int ycolt3 = ycolt2;
+
+            int xcolt4 = xcolt2;
+            int ycolt4 = ycolt1;
+
+            g.DrawLine(blackPen, xcolt1, ycolt1, xcolt3, ycolt3);
+            g.DrawLine(blackPen, xcolt2, ycolt2, xcolt3, ycolt3);
+            g.DrawLine(blackPen, xcolt2, ycolt2, xcolt4, ycolt4);
+            g.DrawLine(blackPen, xcolt4, ycolt4, xcolt1, ycolt1);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_troubleshoot_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(a_txtbox.Text);
+            int b = int.Parse(b_txtbox.Text);
+            int c = int.Parse(c_txtbox.Text);
+            int xcolt1 = Convert.ToInt32(txtbox_colt1x.Text);
+
+            double x = xcolt1;
+            double y = (a * (x * x)) + (b * x) + c;
+
+            string result = Convert.ToString(y);
+
+            lbl_result.Text = result;
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            drawing_board.Refresh();
+            listbox_result.Items.Clear();
+        }
+
+        private void btn_inter_Click(object sender, EventArgs e)
+        {
+            int xcolt1 = Convert.ToInt32(txtbox_colt1x.Text);
+            int ycolt1 = Convert.ToInt32(txtbox_colt1y.Text);
+
+            int xcolt2 = Convert.ToInt32(txtbox_colt2x.Text);
+            int ycolt2 = Convert.ToInt32(txtbox_colt2y.Text);
+
+            int xcolt3 = xcolt1;
+            int ycolt3 = ycolt2;
+
+            int xcolt4 = xcolt2;
+            int ycolt4 = ycolt1;
+
+            int a = int.Parse(a_txtbox.Text);
+            int b = int.Parse(b_txtbox.Text);
+            int c = int.Parse(c_txtbox.Text);
+
+            double x = xcolt1;
+            double y = (a * (x * x)) + (b * x) + c;
+
+            if (y <= Math.Max(ycolt1, ycolt3) && y >= Math.Min(ycolt1, ycolt3))
+            {
+                listbox_result.Items.Add(Convert.ToString(x) + " si " + Convert.ToString(y));
+            }
+
+            x = xcolt2;
+            y = (a * (x * x)) + (b * x) + c;
+
+            if (y <= Math.Max(ycolt2, ycolt4) && y >= Math.Min(ycolt2, ycolt4))
+            {
+                listbox_result.Items.Add(Convert.ToString(x) + " si " + Convert.ToString(y));
+            }
+
+            c += -ycolt1;
+            double x1;
+            double x2;
+
+            double ac = 4 * double.Parse(a_txtbox.Text) * double.Parse(c_txtbox.Text);
+
+            x1 = (-double.Parse(b_txtbox.Text) + Math.Sqrt(Math.Pow(double.Parse(b_txtbox.Text), 2) - ac)) / 2 * double.Parse(a_txtbox.Text);
+            x2 = (-double.Parse(b_txtbox.Text) - Math.Sqrt(Math.Pow(double.Parse(b_txtbox.Text), 2) - ac)) / 2 * double.Parse(a_txtbox.Text);
+
+            if (x1 <= Math.Max(xcolt1, xcolt4) && x1 >= Math.Min(xcolt1, xcolt4))
+            {
+                listbox_result.Items.Add(Convert.ToString(x) + " si " + Convert.ToString(y));
+            }
+            if (x2 <= Math.Max(xcolt1, xcolt4) && x2 >= Math.Min(xcolt1, xcolt4))
+            {
+                listbox_result.Items.Add(Convert.ToString(x) + " si " + Convert.ToString(y));
+            }
+
+            c += -ycolt3; 
+
+            ac = 4 * double.Parse(a_txtbox.Text) * double.Parse(c_txtbox.Text);
+
+            x1 = (-double.Parse(b_txtbox.Text) + Math.Sqrt(Math.Pow(double.Parse(b_txtbox.Text), 2) - ac)) / 2 * double.Parse(a_txtbox.Text);
+            x2 = (-double.Parse(b_txtbox.Text) - Math.Sqrt(Math.Pow(double.Parse(b_txtbox.Text), 2) - ac)) / 2 * double.Parse(a_txtbox.Text);
+
+            if (x1 <= Math.Max(xcolt2, xcolt3) && x1 >= Math.Min(xcolt2, xcolt3))
+            {
+                listbox_result.Items.Add(Convert.ToString(x) + " si " + Convert.ToString(y));
+            }
+            if (x2 <= Math.Max(xcolt2, xcolt3) && x2 >= Math.Min(xcolt2, xcolt3))
+            {
+                listbox_result.Items.Add(Convert.ToString(x) + " si " + Convert.ToString(y));
+            }
+
         }
     }
 }
